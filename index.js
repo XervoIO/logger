47aecc5ec09eccdd5f8252b9793bbc967c83d4b5
Jack Boberg <jack.boberg@gmail.com>
2015-11-23 10:10:13 -0500

    Import existing logger code from other projects

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
-module.exports = function () {
-  // noop
+const Winston = require('winston');
+
+const LOG_LEVEL = process.env.LOG_LEVEL || 'info';
+
+function getFormatter(namespace) {
+  return function (options) {
+    var out = [
+      options.timestamp(),
+      options.level.toUpperCase(),
+      namespace
+    ];
+
+    if (options.message) out.push(options.message);
+    if (options.meta && Object.keys(options.meta).length) {
+      out.push(JSON.stringify(options.meta));
+    }
+
+    return out.join(' ');
+  }
 }
+
+module.exports = function (namespace) {
+  return new Winston.Logger({
+    transports: [
+      new Winston.transports.Console({
+        level: LOG_LEVEL,
+        timestamp: function () {
+          return new Date().toISOString();
+        },
+        formatter: getFormatter(namespace),
+        handleExceptions: true,
+        humanReadableUnhandledException: true
+      })
+    ]
+  });
+};
