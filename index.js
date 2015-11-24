30758691aa2a2aedaa54b652a17bd017b9de6e75
Jack Boberg <jack.boberg@gmail.com>
2015-11-23 22:06:35 -0500

    Extract formatter into local module, and test it

--- index.js
+++ index.js
@@ -1,23 +1,8 @@
 const Winston = require('winston');
 
+const Formatter = require('./lib/formatter');
+
 const LOG_LEVEL = process.env.LOG_LEVEL || 'info';
-
-function getFormatter(namespace) {
-  return function (options) {
-    var out = [
-      options.timestamp(),
-      options.level.toUpperCase(),
-      namespace
-    ];
-
-    if (options.message) out.push(options.message);
-    if (options.meta && Object.keys(options.meta).length) {
-      out.push(JSON.stringify(options.meta));
-    }
-
-    return out.join(' ');
-  }
-}
 
 module.exports = function (namespace) {
   return new Winston.Logger({
@@ -27,7 +12,7 @@ module.exports = function (namespace) {
         timestamp: function () {
           return new Date().toISOString();
         },
-        formatter: getFormatter(namespace),
+        formatter: Formatter.bind(null, namespace),
         handleExceptions: true,
         humanReadableUnhandledException: true
       })
