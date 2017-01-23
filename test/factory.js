c475d33e4c2d6abe5512c2117978d7169194a0d1
Jack Boberg <hello@jackboberg.info>
2017-01-22 22:16:32 -0500

    chore: Convert to standard.js

--- test/factory.js
+++ test/factory.js
@@ -1,198 +1,198 @@
-const Clone = require('lodash.clone');
-const Code = require('code');
-const Lab = require('lab');
-const Sinon = require('sinon');
-const StdMocks = require('std-mocks');
+const Clone = require('lodash.clone')
+const Code = require('code')
+const Lab = require('lab')
+const Sinon = require('sinon')
+const StdMocks = require('std-mocks')
 
-const Factory = require('../lib/factory');
+const Factory = require('../lib/factory')
 
-var lab = exports.lab = Lab.script();
-var describe = lab.describe;
-var it = lab.it;
-var afterEach = lab.afterEach;
-var beforeEach = lab.beforeEach;
-var expect = Code.expect;
+var lab = exports.lab = Lab.script()
+var describe = lab.describe
+var it = lab.it
+var afterEach = lab.afterEach
+var beforeEach = lab.beforeEach
+var expect = Code.expect
 
-const ENV = Clone(process.env, true); // eslint-disable-line no-process-env
+const ENV = Clone(process.env, true)
 
 describe('Factory', function () {
-  var logger, stdio;
+  var logger, stdio
 
   beforeEach(function (done) {
-    logger = Factory('test', 'debug');
-    done();
-  });
+    logger = Factory('test', 'debug')
+    done()
+  })
 
   afterEach(function (done) {
-    process.env = Clone(ENV, true); // eslint-disable-line no-process-env
-    done();
-  });
+    process.env = Clone(ENV, true)
+    done()
+  })
 
   it('exports a function', function (done) {
-    expect(Factory).to.be.a.function();
-    done();
-  });
+    expect(Factory).to.be.a.function()
+    done()
+  })
 
   it('returns a logger instance', function (done) {
     ['error', 'warn', 'info', 'debug'].forEach(function (level) {
-      expect(logger[level]).to.be.a.function();
-    });
-    done();
-  });
+      expect(logger[level]).to.be.a.function()
+    })
+    done()
+  })
 
   describe('instance methods', function () {
     describe('info', function () {
       beforeEach(function (done) {
-        StdMocks.use();
-        logger.info('message');
-        StdMocks.restore();
-        stdio = StdMocks.flush();
-        done();
-      });
+        StdMocks.use()
+        logger.info('message')
+        StdMocks.restore()
+        stdio = StdMocks.flush()
+        done()
+      })
 
       it('prints to stdout', function (done) {
-        expect(stdio.stdout.pop()).to.contain('INFO test message');
-        done();
-      });
-    });
+        expect(stdio.stdout.pop()).to.contain('INFO test message')
+        done()
+      })
+    })
 
     describe('warn', function () {
       beforeEach(function (done) {
-        StdMocks.use();
-        logger.warn('message');
-        StdMocks.restore();
-        stdio = StdMocks.flush();
-        done();
-      });
+        StdMocks.use()
+        logger.warn('message')
+        StdMocks.restore()
+        stdio = StdMocks.flush()
+        done()
+      })
 
       it('prints to stdout', function (done) {
-        expect(stdio.stdout.pop()).to.contain('WARN test message');
-        done();
-      });
-    });
+        expect(stdio.stdout.pop()).to.contain('WARN test message')
+        done()
+      })
+    })
 
     describe('error', function () {
       beforeEach(function (done) {
-        StdMocks.use();
-        logger.error('message');
-        StdMocks.restore();
-        stdio = StdMocks.flush();
-        done();
-      });
+        StdMocks.use()
+        logger.error('message')
+        StdMocks.restore()
+        stdio = StdMocks.flush()
+        done()
+      })
 
       it('prints to stderr', function (done) {
-        expect(stdio.stderr.pop()).to.contain('ERROR test message');
-        done();
-      });
-    });
+        expect(stdio.stderr.pop()).to.contain('ERROR test message')
+        done()
+      })
+    })
 
     describe('debug', function () {
       beforeEach(function (done) {
-        StdMocks.use();
-        logger.debug('message');
-        StdMocks.restore();
-        stdio = StdMocks.flush();
-        done();
-      });
+        StdMocks.use()
+        logger.debug('message')
+        StdMocks.restore()
+        stdio = StdMocks.flush()
+        done()
+      })
 
       it('prints to stderr', function (done) {
-        expect(stdio.stderr.pop()).to.contain('DEBUG test message');
-        done();
-      });
-    });
+        expect(stdio.stderr.pop()).to.contain('DEBUG test message')
+        done()
+      })
+    })
 
     describe('exceptions', function () {
       afterEach(function (done) {
-        process.exit.restore();
-        done();
-      });
+        process.exit.restore()
+        done()
+      })
 
       describe('when in production environment', function () {
         beforeEach(function (done) {
-          process.env.NODE_ENV = 'production'; // eslint-disable-line no-process-env
+          process.env.NODE_ENV = 'production'
 
-          Sinon.stub(process, 'exit');
+          Sinon.stub(process, 'exit')
 
-          StdMocks.use();
-          process.emit('uncaughtException', new Error('uncaught'));
-          StdMocks.restore();
-          stdio = StdMocks.flush();
+          StdMocks.use()
+          process.emit('uncaughtException', new Error('uncaught'))
+          StdMocks.restore()
+          stdio = StdMocks.flush()
 
-          done();
-        });
+          done()
+        })
 
         it('prints to stderr', function (done) {
-          var log = stdio.stderr.pop();
-          expect(log).to.contain('uncaughtException');
-          expect(log).to.not.match(/logger\/test\/factory.js:(?:\d+):(?:\d+)\n/);
-          done();
-        });
-      });
+          var log = stdio.stderr.pop()
+          expect(log).to.contain('uncaughtException')
+          expect(log).to.not.match(/logger\/test\/factory.js:(?:\d+):(?:\d+)\n/)
+          done()
+        })
+      })
 
       describe('when not in production environment', function () {
         beforeEach(function (done) {
-          Sinon.stub(process, 'exit');
+          Sinon.stub(process, 'exit')
 
-          StdMocks.use();
-          process.emit('uncaughtException', new Error('uncaught'));
-          StdMocks.restore();
-          stdio = StdMocks.flush();
+          StdMocks.use()
+          process.emit('uncaughtException', new Error('uncaught'))
+          StdMocks.restore()
+          stdio = StdMocks.flush()
 
-          done();
-        });
+          done()
+        })
 
         it('prints to stderr with stack trace', function (done) {
-          var log = stdio.stderr.pop();
-          expect(log).to.contain('uncaughtException');
-          expect(log).to.match(/logger\/test\/factory.js:(?:\d+):(?:\d+)\n/);
-          done();
-        });
-      });
-    });
-  });
+          var log = stdio.stderr.pop()
+          expect(log).to.contain('uncaughtException')
+          expect(log).to.match(/logger\/test\/factory.js:(?:\d+):(?:\d+)\n/)
+          done()
+        })
+      })
+    })
+  })
 
   describe('process exit', function () {
     beforeEach(function (done) {
-      Sinon.stub(process, 'exit');
-      done();
-    });
+      Sinon.stub(process, 'exit')
+      done()
+    })
 
     afterEach(function (done) {
-      process.exit.restore();
-      done();
-    });
+      process.exit.restore()
+      done()
+    })
 
     describe('when created without the noExit param', function () {
       beforeEach(function (done) {
-        StdMocks.use();
-        process.emit('uncaughtException', new Error('uncaught'));
-        StdMocks.restore();
+        StdMocks.use()
+        process.emit('uncaughtException', new Error('uncaught'))
+        StdMocks.restore()
 
-        done();
-      });
+        done()
+      })
 
       it('exits the process', function (done) {
-        expect(process.exit.calledWith(1)).to.be.true();
-        done();
-      });
-    });
+        expect(process.exit.calledWith(1)).to.be.true()
+        done()
+      })
+    })
 
     describe('when created with the noExit param', function () {
       beforeEach(function (done) {
-        process.removeAllListeners('uncaughtException');
-        logger = Factory('test', 'debug', false);
+        process.removeAllListeners('uncaughtException')
+        logger = Factory('test', 'debug', false)
 
-        StdMocks.use();
-        process.emit('uncaughtException', new Error('other'));
-        StdMocks.restore();
+        StdMocks.use()
+        process.emit('uncaughtException', new Error('other'))
+        StdMocks.restore()
 
-        done();
-      });
+        done()
+      })
 
       it('does not exit the process', function (done) {
-        expect(process.exit.called).to.be.false();
-        done();
-      });
-    });
-  });
-});
+        expect(process.exit.called).to.be.false()
+        done()
+      })
+    })
+  })
+})
--- test/logger.js
+++ test/logger.js
@@ -1,194 +1,194 @@
-const FS = require('fs');
-const Path = require('path');
-const OS = require('os');
+const FS = require('fs')
+const Path = require('path')
+const OS = require('os')
 
-const Code = require('code');
-const Clone = require('lodash.clone');
-const Lab = require('lab');
-const Proxyquire = require('proxyquire');
-const Sinon = require('sinon');
-const StdMocks = require('std-mocks');
+const Code = require('code')
+const Clone = require('lodash.clone')
+const Lab = require('lab')
+const Proxyquire = require('proxyquire')
+const Sinon = require('sinon')
+const StdMocks = require('std-mocks')
 
-var mock = { logger: true };
-var factory = Sinon.stub().returns(mock);
+var mock = { logger: true }
+var factory = Sinon.stub().returns(mock)
 const Logger = Proxyquire('..', {
   './lib/factory': factory
-});
+})
 
-var lab = exports.lab = Lab.script();
-var describe = lab.describe;
-var it = lab.it;
-var beforeEach = lab.beforeEach;
-var afterEach = lab.afterEach;
-var expect = Code.expect;
+var lab = exports.lab = Lab.script()
+var describe = lab.describe
+var it = lab.it
+var beforeEach = lab.beforeEach
+var afterEach = lab.afterEach
+var expect = Code.expect
 
-const ENV = Clone(process.env, true); // eslint-disable-line no-process-env
+const ENV = Clone(process.env, true)
 
 describe('Logger', function () {
-  var fn;
+  var fn
 
   afterEach(function (done) {
-    factory.reset();
-    process.env = Clone(ENV, true); // eslint-disable-line no-process-env
-    done();
-  });
+    factory.reset()
+    process.env = Clone(ENV, true)
+    done()
+  })
 
   it('exports a function', function (done) {
-    expect(Logger).to.be.a.function();
-    done();
-  });
+    expect(Logger).to.be.a.function()
+    done()
+  })
 
   it('requires a namespace', function (done) {
     fn = function () {
-      Logger();
-    };
+      Logger()
+    }
 
-    expect(fn).to.throw('must provide namespace');
-    done();
-  });
+    expect(fn).to.throw('must provide namespace')
+    done()
+  })
 
   it('namespace must be string', function (done) {
     [void 0, null, [], {}, Function.prototype].forEach(function (el) {
       fn = function () {
-        Logger(el);
-      };
-      expect(fn).to.throw('must provide namespace');
-    });
-    done();
-  });
+        Logger(el)
+      }
+      expect(fn).to.throw('must provide namespace')
+    })
+    done()
+  })
 
   it('returns a logger instance', function (done) {
-    var instance = Logger('test');
-    expect(factory.calledWith('test')).to.be.true();
-    expect(instance).to.equal(mock);
-    done();
-  });
+    var instance = Logger('test')
+    expect(factory.calledWith('test')).to.be.true()
+    expect(instance).to.equal(mock)
+    done()
+  })
 
   describe('log level', function () {
     describe('when environment variable is not present', function () {
       beforeEach(function (done) {
-        delete process.env.LOG_LEVEL; // eslint-disable-line no-process-env
-        done();
-      });
+        delete process.env.LOG_LEVEL
+        done()
+      })
 
       it('defaults to "info"', function (done) {
-        Logger('namespace');
-        expect(factory.calledWith('namespace', 'info')).to.be.true();
-        done();
-      });
-    });
+        Logger('namespace')
+        expect(factory.calledWith('namespace', 'info')).to.be.true()
+        done()
+      })
+    })
 
     describe('when environment variable is present', function () {
       beforeEach(function (done) {
-        process.env.LOG_LEVEL = 'test'; // eslint-disable-line no-process-env
-        done();
-      });
+        process.env.LOG_LEVEL = 'test'
+        done()
+      })
 
       it('uses it', function (done) {
-        Logger('namespace');
-        expect(factory.calledWith('namespace', 'test')).to.be.true();
-        done();
-      });
-    });
-  });
+        Logger('namespace')
+        expect(factory.calledWith('namespace', 'test')).to.be.true()
+        done()
+      })
+    })
+  })
 
   describe('writeExceptions', function () {
     it('requires a path', function (done) {
       fn = function () {
-        Logger.writeExceptions();
-      };
+        Logger.writeExceptions()
+      }
 
-      expect(fn).to.throw('must provide a file path');
-      done();
-    });
+      expect(fn).to.throw('must provide a file path')
+      done()
+    })
 
     it('path must be string', function (done) {
       [void 0, null, [], {}, Function.prototype].forEach(function (el) {
         fn = function () {
-          Logger.writeExceptions(el);
-        };
-        expect(fn).to.throw('must provide a file path');
-      });
-      done();
-    });
+          Logger.writeExceptions(el)
+        }
+        expect(fn).to.throw('must provide a file path')
+      })
+      done()
+    })
 
     describe('when no exception log is not set', function () {
       it('creates loggers that exit on exceptions', function (done) {
-        Logger('namespace');
-        expect(factory.lastCall.args.pop()).to.be.true();
-        done();
-      });
-    });
+        Logger('namespace')
+        expect(factory.lastCall.args.pop()).to.be.true()
+        done()
+      })
+    })
 
     describe('when exception log is set', function () {
-      var logPath;
+      var logPath
 
       beforeEach(function (done) {
-        logPath = Path.join(OS.tmpdir(), 'exception.log');
-        process.env.EXCEPTION_LOG = logPath; // eslint-disable-line no-process-env, max-len
-        Sinon.stub(process, 'exit');
+        logPath = Path.join(OS.tmpdir(), 'exception.log')
+        process.env.EXCEPTION_LOG = logPath
+        Sinon.stub(process, 'exit')
 
         FS.writeFile(logPath, null, function (err) {
-          if (err) return done(err);
+          if (err) return done(err)
 
           FS.access(logPath, FS.OK | FS.W_OK, function (err) {
-            if (err) return done(err);
+            if (err) return done(err)
 
-            Logger.writeExceptions(logPath);
+            Logger.writeExceptions(logPath)
 
-            StdMocks.use();
-            process.emit('uncaughtException', new Error('uncaught'));
-            StdMocks.restore();
-            StdMocks.flush();
+            StdMocks.use()
+            process.emit('uncaughtException', new Error('uncaught'))
+            StdMocks.restore()
+            StdMocks.flush()
 
-            done();
-          });
-        });
-      });
+            done()
+          })
+        })
+      })
 
       afterEach(function (done) {
-        process.exit.restore();
-        FS.unlink(logPath, done);
-      });
+        process.exit.restore()
+        FS.unlink(logPath, done)
+      })
 
       it('creates loggers that do not exit on exceptions', function (done) {
-        Logger('namespace');
-        expect(factory.lastCall.args.pop()).to.be.false();
-        done();
-      });
+        Logger('namespace')
+        expect(factory.lastCall.args.pop()).to.be.false()
+        done()
+      })
 
       describe('when log is writeable', function () {
         it('writes exceptions to file', function (done) {
           FS.readFile(logPath, 'utf8', function (err, log) {
-            if (err) return done(err);
+            if (err) return done(err)
 
-            expect(log).to.contain('uncaughtException');
-            expect(log).to.contain('Error: uncaught');
-            done();
-          });
-        });
-      });
+            expect(log).to.contain('uncaughtException')
+            expect(log).to.contain('Error: uncaught')
+            done()
+          })
+        })
+      })
 
       describe('when log is not writeable', function () {
         beforeEach(function (done) {
-          Sinon.stub(FS, 'appendFileSync').throws();
-          done();
-        });
+          Sinon.stub(FS, 'appendFileSync').throws()
+          done()
+        })
 
         afterEach(function (done) {
-          FS.appendFileSync.restore(); // eslint-disable-line no-sync
-          done();
-        });
+          FS.appendFileSync.restore()
+          done()
+        })
 
         it('throws an exception', function (done) {
           fn = function () {
-            Logger.writeExceptions(logPath);
-          };
+            Logger.writeExceptions(logPath)
+          }
 
-          expect(fn).to.throw();
-          done();
-        });
-      });
-    });
-  });
-});
+          expect(fn).to.throw()
+          done()
+        })
+      })
+    })
+  })
+})
